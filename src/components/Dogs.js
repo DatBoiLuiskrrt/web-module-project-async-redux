9d747194b4c6ae08d121f2231f54afb5403d4d02
datboiluiskrrt <datboiluiskrrt@gmail.com>
2022-01-19 17:47:42 -0600

    Added some styling

--- src/components/Dogs.js
+++ src/components/Dogs.js
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { fetchFail, fetchStart, fetchSuccess } from "../actions";
 import axios from "axios";
-
+// import "./index.css";
 import { getDogs } from "../actions/index";
 
 const Dogs = ({ error, dispatch, isFetching, message }) => {
@@ -14,16 +14,21 @@ const Dogs = ({ error, dispatch, isFetching, message }) => {
   if (error) {
     return <h2>We got an weeoe: {error}</h2>;
   }
+  if (isFetching) {
+    return <h2>Fetching a doggie for you</h2>;
+  }
 
   const handleClick = () => {
     dispatch(getDogs());
   };
   return (
-    <>
+    <div className="card-container">
       <h2>Say hi to: </h2>
       <img src={message.message} />
-      <button onClick={handleClick}>Get a new Doggie</button>
-    </>
+      <button className="button" onClick={handleClick}>
+        Get a new Doggie
+      </button>
+    </div>
   );
 };
 
